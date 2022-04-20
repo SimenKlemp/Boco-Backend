2b476b79f25ceabe51fb6312d7c8ef704363587c
Vetle Malmin Skretting <vetlems@stud.ntnu.no>
2022-04-20 10:57:02 +0200

    Added swagger

---
+++ src/main/java/edu/ntnu/idatt2106/boco/swagger/SwaggerConfig.java
@@ -0,0 +1,58 @@
+package edu.ntnu.idatt2106.boco.swagger;
+
+import com.google.common.collect.Lists;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig
+{
+    @Bean
+    public Docket api()
+    {
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(ApiInfo.DEFAULT)
+                .forCodeGeneration(true)
+                .securityContexts(Lists.newArrayList(securityContext()))
+                .securitySchemes(Lists.newArrayList(apiKey()))
+                .useDefaultResponseMessages(false);
+
+        docket = docket.select()
+                .apis(RequestHandlerSelectors.basePackage("edu.ntnu.idatt2106.boco"))
+                .build();
+
+        return docket;
+    }
+
+    private ApiKey apiKey()
+    {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext()
+    {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    List<SecurityReference> defaultAuth()
+    {
+        AuthorizationScope authorizationScope
+                = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Lists.newArrayList(
+                new SecurityReference("JWT", authorizationScopes));
+    }
+}
