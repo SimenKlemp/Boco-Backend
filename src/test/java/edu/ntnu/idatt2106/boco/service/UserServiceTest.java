55aacf5d05ecc0c39d5ecd0b6988ff9799ccba6d
Marius Klemp Petersen <mariuskp@stud.ntnu.no>
2022-04-21 13:49:49 +0200

    Updated test service

--- src/test/java/edu/ntnu/idatt2106/boco/service/UserServiceTest.java
+++ src/test/java/edu/ntnu/idatt2106/boco/service/UserServiceTest.java
@@ -32,6 +32,7 @@ public class UserServiceTest {
 
         Mockito.lenient().when(userRepository.save(Mockito.any())).thenReturn(user);
         Mockito.lenient().when(userRepository.existsByEmail(Mockito.anyString())).thenReturn(false);
+        Mockito.lenient().when(userRepository.findByEmail(Mockito.anyString())).thenReturn(java.util.Optional.of(user));
     }
     @Test
     void registerTest() {
@@ -42,10 +43,24 @@ public class UserServiceTest {
 
     @Test
     void deleteUserByEmailTest() {
-        RegisterUserRequest request = new RegisterUserRequest("Name",true, "Address", "Email", "Password");
+        User user = new User("Name", true, "Address", "Email", "Password", "Admin");
 
-        User user = userService.register(request);
+        boolean response = userService.deleteUserByEmail(user);
+
+        assertThat(response).isTrue();
     }
+
+    @Test
+    void updateUserByEmailTest() {
+        User user = new User("Name", true, "Address", "Email", "Password", "Admin");
+
+        boolean response = userService.updateUserByEmail("NewEmail.com", user);
+
+        assertThat(response).isTrue();
+    }
+
+
+
 
 
 }
