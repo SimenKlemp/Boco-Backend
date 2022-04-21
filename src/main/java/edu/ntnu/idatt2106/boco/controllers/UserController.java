1c303825aee4bf8075fe864c92fcf816a1f0b76c
Mona Mahmoud Mousa <monamm@stud.ntnu.no>
2022-04-21 11:06:56 +0200

    Modified userController class

--- src/main/java/edu/ntnu/idatt2106/boco/controllers/UserController.java
+++ src/main/java/edu/ntnu/idatt2106/boco/controllers/UserController.java
@@ -5,11 +5,9 @@ import javax.validation.Valid;
 import edu.ntnu.idatt2106.boco.token.TokenComponent;
 import edu.ntnu.idatt2106.boco.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import edu.ntnu.idatt2106.boco.models.User;
 import edu.ntnu.idatt2106.boco.payload.request.LoginRequest;
@@ -54,4 +52,32 @@ public class UserController
             user.getRole()
     );
   }
+
+  @DeleteMapping(value="/user/delete")
+  public ResponseEntity<String> deleteUserByEmail(@RequestBody User user ){
+    try{
+
+      if( userService.deleteUserByEmail(user)){
+        return new ResponseEntity("Error: User not found", HttpStatus.OK);
+      }
+      return new ResponseEntity("User has been deleted successfully", HttpStatus.OK);
+    }catch (Exception ex){
+      return new ResponseEntity("Error: Can not delete ",HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @PutMapping (value="/user/update")
+  public ResponseEntity<String> updateUserInfo(@PathVariable("email")String email, @RequestBody User user) {
+    try {
+
+      if (userService.updateUserByEmail(email,user)) {
+        return new ResponseEntity("Error: User not found", HttpStatus.OK);
+      }
+
+      return new ResponseEntity("User has been updated",HttpStatus.OK);
+    } catch (Exception ex) {
+      return new ResponseEntity("Can not update", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
 }
