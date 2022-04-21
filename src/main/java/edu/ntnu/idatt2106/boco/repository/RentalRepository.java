c0c4a0867d7041fe77d7fd76fd787571fcba7d59
Simen Klemp <simenk2312@gmail.com>
2022-04-21 13:33:24 +0200

    Created method for retrieving all rentalRequests for a specific item

--- src/main/java/edu/ntnu/idatt2106/boco/repository/RentalRepository.java
+++ src/main/java/edu/ntnu/idatt2106/boco/repository/RentalRepository.java
@@ -4,6 +4,10 @@ import edu.ntnu.idatt2106.boco.models.Item;
 import edu.ntnu.idatt2106.boco.models.Rental;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RentalRepository extends JpaRepository<Rental, Long> {
+
+    List<Rental> findAllByItemId(Long itemId);
 
 }
