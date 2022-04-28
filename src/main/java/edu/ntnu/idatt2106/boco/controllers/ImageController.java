679b758156b2ca5bcacd46e0ce5d21a1fb2a937a
Vetle Malmin Skretting <vetlems@stud.ntnu.no>
2022-04-28 10:15:40 +0200

    Fixed image upload (hopefully)

--- src/main/java/edu/ntnu/idatt2106/boco/controllers/ImageController.java
+++ src/main/java/edu/ntnu/idatt2106/boco/controllers/ImageController.java
@@ -1,6 +1,8 @@
 package edu.ntnu.idatt2106.boco.controllers;
 
 import edu.ntnu.idatt2106.boco.service.ImageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.core.io.Resource;
@@ -17,6 +19,9 @@ class ImageController
     @Autowired
     ImageService imageService;
 
+    Logger logger = LoggerFactory.getLogger(ImageController.class);
+
+
     @GetMapping(value = "/{imageId}", produces = MediaType.IMAGE_JPEG_VALUE)
     Resource getImage(@PathVariable long imageId)
     {
@@ -26,6 +31,7 @@ class ImageController
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     long uploadImage(@RequestParam("image") MultipartFile image)
     {
+        logger.info("Uploading image");
         return imageService.upload(image);
     }
 }
