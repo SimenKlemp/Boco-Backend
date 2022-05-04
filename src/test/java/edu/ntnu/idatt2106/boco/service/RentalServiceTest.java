6d3ea1eff4fc3bbadbf5dc06253df4271a81e72a
Mona Mahmoud Mousa <monamm@stud.ntnu.no>
2022-05-04 14:46:28 +0200

    updating tokenComponentTest

--- src/test/java/edu/ntnu/idatt2106/boco/service/RentalServiceTest.java
+++ src/test/java/edu/ntnu/idatt2106/boco/service/RentalServiceTest.java
@@ -17,8 +17,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
--- src/test/java/edu/ntnu/idatt2106/boco/token/TokenComponentTest.java
+++ src/test/java/edu/ntnu/idatt2106/boco/token/TokenComponentTest.java
@@ -1,16 +1,26 @@
 package edu.ntnu.idatt2106.boco.token;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.ntnu.idatt2106.boco.factories.modelFactroies.UserFactory;
+import edu.ntnu.idatt2106.boco.models.User;
+import edu.ntnu.idatt2106.boco.repository.UserRepository;
+import edu.ntnu.idatt2106.boco.util.RepositoryMock;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 
 @SpringBootTest(
@@ -21,21 +31,35 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class TokenComponentTest {
 
     @Autowired
-    TokenComponent tokenComponent;
-    protected MockMvc mvc;
+    private TokenComponent tokenComponent;
+    @Autowired
+    private MockMvc mvc;
+
+    @Mock
+    private UserRepository userRepository;
+
+    User user =new User();
+    @BeforeEach
+    public void setUp() throws Exception {
+        RepositoryMock.mockUserRepository(userRepository);
+        User user=new UserFactory().getObject();
+        userRepository.save(user);
+    }
 
     @Test
     public void shouldNotAllowAccessToUnauthenticatedUsers() throws Exception {
-        mvc.perform(get("/user")).andExpect(status().isForbidden());
+        mvc.perform(get("/user")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(new ObjectMapper().writeValueAsString(user)))
+                .andExpect(status().isUnauthorized());
     }
 
     @Test
     public void shouldGenerateAuthToken() throws Exception {
-        String token = tokenComponent.generateToken(1L,"ADMIN");
-
+        user.setRole("ADMIN");
+        userRepository.save(user);
+        String token =tokenComponent.generateToken(user.getUserId(), user.getRole());
         assertNotNull(token);
-        mvc.perform(get("/user").header("Authorization", token))
-                .andExpect(status().isOk());
     }
 
     /**
@@ -44,9 +68,12 @@ class TokenComponentTest {
      */
 
     @Test
-    @WithMockUser(username = "email", password = "password", roles = {"USER","ADMIN"})
     void isThisUserTest() throws Exception {
-        this.mvc.perform(get("/user")).andExpect(status().isOk());
+        user.setRole("USER");
+        userRepository.save(user);
+        tokenComponent.generateToken(user.getUserId(), user.getRole());
+        Boolean isUser=tokenComponent.haveAccessTo(user.getUserId());
+        assertThat(isUser).isEqualTo(true);
     }
 
     /**
@@ -56,6 +83,9 @@ class TokenComponentTest {
     @Test
     @WithMockUser(username = "email", password = "password", roles = {"ADMIN"})
     void isAdminTest() throws Exception {
-        this.mvc.perform(get("/user")).andExpect(status().isOk());
+        user.setRole("ADMIN");
+        userRepository.save(user);
+        Boolean isAdmin=tokenComponent.haveAccessTo(user.getUserId());
+        assertThat(isAdmin).isEqualTo(true);
     }
 }
